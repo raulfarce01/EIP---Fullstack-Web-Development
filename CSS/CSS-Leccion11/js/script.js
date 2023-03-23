d758f83c8470d8ca5c463a401f4d69de2ab09f29
raulfarce01 <raulito72001@gmail.com>
2023-03-23 14:33:10 +0100

    Lecciones y Ej 2

---
+++ CSS/CSS-Leccion11/js/script.js
@@ -0,0 +1,5 @@
+const toggleMenu = () => {
+  document.getElementById("menu-mob").classList.toggle("menu-open");
+  document.getElementById("burger").classList.toggle("is-active");
+  document.getElementById("darkbg").classList.toggle("visible");
+};
