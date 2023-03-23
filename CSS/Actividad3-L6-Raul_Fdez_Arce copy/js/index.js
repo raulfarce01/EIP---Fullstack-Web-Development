6bbb419b0a320966d790d529849e0bdc67c2671f
raulfarce01 <raulito72001@gmail.com>
2023-03-23 15:15:36 +0100

    Ejercicio 3

---
+++ CSS/Actividad3-L6-Raul_Fdez_Arce copy/js/index.js
@@ -0,0 +1,13 @@
+var hora = document.getElementById("hora");
+
+function horaConstante() {
+    var fecha = new Date();
+
+    var now = fecha.toLocaleTimeString("es-ES");
+
+    hora.innerHTML = now;
+
+
+};
+
+setInterval(horaConstante, 1000);
